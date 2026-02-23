1582117c6b93660c801a1823d645622ab4d984a3
simon clavagnier <simondelph.cla@gmail.com>
2026-02-23 09:52:05 +0100

    allow easy addition of new atlas in an already existing specie template

--- atlases/add_atlas_to_spec.py
+++ atlases/add_atlas_to_spec.py
@@ -3,6 +3,8 @@ import os
 import glob
 import ants
 
+
+
 opj = os.path.join
 opb = os.path.basename
 opd = os.path.dirname
@@ -10,12 +12,19 @@ ope = os.path.exists
 opi = os.path.isfile
 
 from Tools import run_cmd
-from Tools import getpath
-from anat import transfo_T2toT1w
 from anat.connectomeWB import WB_label
+from atlases import templatefeat
+
+T1interp    = 'hammingWindowedSinc'
+labelinterp = 'genericLabel'
+
+antsaff     = '0GenericAffine.mat'
+antswarp    = '1Warp.nii.gz'
+antsinvwarp = '1InverseWarp.nii.gz'
 
 
-def add(cmd_mris,atlas,template,doT,label,name,LR,diary_name):
+def add(specie,animal,newAtlasfile,newAtlasAnatfile,labelname,sameAnimal,doT,wFS,LR,diary_name,path_ATLAS, fs_tools,cmd_mris,sing_afni,sing_fs,export_fs,sing_wb):
+
     '''
     add_atlas_to_spec takes a labelled volume file fr oma an atlas and create a new volume file readable for WB and the corresponding labelled surfaces
     Parameters
@@ -25,7 +34,7 @@ def add(cmd_mris,atlas,template,doT,label,name,LR,diary_name):
     template : the destination of the new atlas
     name
     T_transfo: do you need to change the T1 into a T2
-    doT :[yesorno,source,existornot,T_transfo,msk,type] type could 'Translation','Rigid','SyNCC'
+    doT :[yes-or-no,source,exist-or-not,T_transfo,msk,type] type could 'Translation','Rigid','SyNCC'
     label the name of the atlas
     diary_name : where to write the processings steps
     Returns
@@ -33,87 +42,109 @@ def add(cmd_mris,atlas,template,doT,label,name,LR,diary_name):
     '''
 
     nl = 'add atlas to specfile'
-    run_cmd.msg(nl, diary_name,'OKGREEN')
+    run_cmd.msg(nl, diary_name)
 
-    main_path = opd(opd(opd(template)))
-    dir_path  = opd(template)
-    animal    = opb(template).split('_')[0]
+    [refDir,refile ] = templatefeat.get(specie, path_ATLAS, fs_tools,
+                                         animal, '', '', 'wb_atlas', 'anat')
 
-    check = glob.glob(opj(main_path,'preprocessing','*_resamp-*'))
-    if check == []:
-        change_hd = 0
-    else:
-        change_hd = 1
 
-    FS_dir   = opj(main_path,'freesurfer')
-    WB_dir   = opj(main_path,'native','surfaces','Native_resol')
+    # Targetparams:
+
+    matrice_dir   = opj(refDir,'matrices')
+    refFS_dir     = opj(refDir, 'freesurfer')
+    reflabeldir = opj(refDir,'volumes','labels')
+    check         = glob.glob(opj(refDir, 'preprocessing', '*_resamp-*'))
+    refWB_dir     = opj(refDir,'surfaces','Native_resol')
     surftype = '.native'
-    roi = 'roi'
-    spec     = opj(main_path,'native', 'surfaces', 'Native_resol', animal + '_native_LR.wb.spec')
-
-    if LR ==0:
-        suffix = '_StatsLUT.txt'
-    else:
-        suffix = '_l.ctab'
+    roi      = 'roi'
+    spec = opj(refWB_dir, animal + '_native_LR.wb.spec')
 
 
-    if doT[0] == 1:
+    Tmode         = 'SyNCC' # use 'SyN' with syn_metric='mattes' for T2/T1
 
-        if doT[2] == 0:
-            source = doT[1]
-            if not ope(opj(main_path, 'matrices')):
-                os.makedirs(opj(main_path, 'matrices'))
-            if doT[3] == 1:
-                transfo_T2toT1w.prepa(source, doT[4], '', diary_name)
-                source = source.replace('_T2w.nii.gz','_T1w.nii.gz')
+    #newAtlaslabel   is the file readable by workbench' :blabla_label.txt'
+    #labelname : the name you want to give to the new atlas ex 'BAL2026'
 
-            orig = ants.image_read(source)
-            new  = ants.image_read(template)
-            ants.registration(fixed=orig, moving=new,
-                                type_of_transform=doT[5],
-                                initial_transform=None,
-                                outprefix=opj(main_path, 'matrices', 'from_' + name + '_'))
 
-            if type == 'SyNCC':
-                transfo = [opj(main_path, 'matrices', 'from_' + name + '_1InverseWarp.nii.gz'),
-                           opj(main_path, 'matrices', 'from_' + name + '_0GenericAffine.mat')]
-                w2i     = [False,True]
-            else:
-                transfo = opj(main_path, 'matrices', 'from_' + name + '_0GenericAffine.mat')
-                w2i     = [True]
+    newAtlas2ref  = '-'.join(['from',labelname])
+    newAtlas2refT = {'Tname' :opj(matrice_dir, '_'.join([newAtlas2ref,''])),
+                     'affine': opj(matrice_dir, '_'.join([newAtlas2ref,antsaff])),
+                     'warp'  :   opj(matrice_dir, '_'.join([newAtlas2ref,antswarp])),
+                     'invwarp':opj(matrice_dir, '_'.join([newAtlas2ref,antsinvwarp])),
+                     'type':Tmode}
 
-            WB_label.vol(atlas, template, spec, transfo, w2i, label + '_label.txt', diary_name)
+    if doT == 1:
+        if not opi(newAtlas2refT['affine']):
+            if not ope(matrice_dir):
+                os.makedirs(matrice_dir)
 
-        elif doT[2] == 1 and name == 'to_ac':
-            transfo = opj(main_path, 'matrices', 'to_ac_0GenericAffine.mat')
-            w2i = [True]
-            WB_label.vol(atlas, template, spec, transfo, w2i, label + '_label.txt', diary_name)
+            source = ants.image_read(newAtlasAnatfile)
+            target = ants.image_read(refile)
 
-        else :
-            if opi(opj(main_path, 'matrices', 'from_' + name + '_0GenericAffine.mat'))==True:
-                if opi(opj(main_path, 'matrices', 'from_' + name + '_1InverseWarp.nii.gz')):
-                    transfo = [opj(main_path, 'matrices', 'from_' + name + '_1InverseWarp.nii.gz'),
-                               opj(main_path, 'matrices', 'from_' + name + '_0GenericAffine.mat')]
-                    w2i = [False,True]
+            if not sameAnimal:
 
-                    #transfo = [opj(main_path, 'matrices', 'from_' + name + '_0GenericAffine.mat'),
-                    #           opj(main_path, 'matrices', 'from_' + name + '_1Warp.nii.gz')]
-                    #w2i = [False, False]
-                else :
-                    transfo = opj(main_path, 'matrices', 'from_' + name + '_0GenericAffine.mat')
+                ants.registration(fixed=source, moving=target,
+                                  type_of_transform='Translation',outprefix=newAtlas2refT['Tname'])
+                ants.registration(fixed=source, moving=target,
+                                    type_of_transform=Tmode,
+                                    initial_transform=newAtlas2refT['affine'],
+                                    outprefix=newAtlas2refT['Tname'])
+
+                if opi(newAtlas2refT['invwarp']):
+                    transfo = [newAtlas2refT['affine'],newAtlas2refT['invwarp']]
+                    w2i     = [True,False]
+                else:
+                    transfo = newAtlas2refT['affine']
                     w2i     = [True]
 
-            WB_label.vol(atlas, template, spec, transfo, w2i, label + '_label.txt', diary_name)
+                WB_label.vol(newAtlasfile, refile, reflabeldir,spec, transfo, w2i, opj(path_code_label,labelname + '_label.txt'), diary_name,sing_wb)
 
-    else :
-        WB_label.vol(atlas, template, spec, '', '', label + '_label.txt', diary_name)
+            else:
+                ants.registration(fixed=source, moving=target,
+                                  type_of_transform='Translation', outprefix=newAtlas2refT['Tname'])
+                ants.registration(fixed=source, moving=target,
+                                  type_of_transform='Rigid',
+                                  initial_transform=newAtlas2refT['affine'],
+                                  outprefix=newAtlas2refT['Tname'])
 
-    WB_label.surfFS(opj(dir_path, 'labels', animal + '_seg-' + opb(label).split('_')[0] + '_dseg.nii.gz'),
-                    label + suffix,
-                    change_hd, diary_name)
+                transfo = newAtlas2refT['affine']
+                w2i = [True]
+                WB_label.vol(newAtlasfile, refile, reflabeldir,spec, transfo, w2i, opj(path_code_label,labelname + '_label.txt'), diary_name,sing_wb)
 
-    WB_label.surfWB(cmd_mris, FS_dir, animal, opb(label).split('_')[0], WB_dir, surftype, diary_name)
-    WB_label.cifti(animal, opb(label).split('_')[0], WB_dir, surftype, roi, spec, diary_name)
+        else :
+            if opi(newAtlas2refT['invwarp']):
+                transfo = [newAtlas2refT['affine'],newAtlas2refT['invwarp']]
+                w2i     = [True,False]
+            else:
+                transfo = newAtlas2refT['affine']
+                w2i     = [True]
+
+        WB_label.vol(newAtlasfile, refile, reflabeldir,spec, transfo, w2i, opj(path_code_label,labelname + '_label.txt'), diary_name,sing_wb)
+
+    if wFS == 1:
+        if not check:
+            change_hd = 0
+        else:
+            change_hd = 1
+
+        if LR == 0:
+            suffix = 'StatsLUT.txt'
+        else:
+            suffix = 'l.ctab'
+        WB_label.surfFS(opj(reflabeldir, animal + '_seg-' + labelname + '_dseg.nii.gz'),
+                        opj(path_code_label,'_'.join([labelname,suffix])),
+                        change_hd, diary_name,sing_fs,path_code_label,export_fs,sing_afni,sing_wb)
+
+        WB_label.surfWB(cmd_mris , refFS_dir, animal, labelname, refWB_dir, surftype, diary_name)
+    else:
+        for h in [0,1]:
+            WB_label.surfvolWB(newAtlasfile, animal, labelname, refWB_dir, surftype, h, diary_name, sing_wb)
+
+    WB_label.cifti(animal, labelname, refWB_dir, surftype,roi, spec, diary_name)
+
+
+
+
 
 
 
